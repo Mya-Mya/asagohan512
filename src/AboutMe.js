aabbceeb25cc613064abab453aebdba4d8830335
Mya-Mya <interleukin413@gmail.com>
2020-09-18 20:43:16 +0900

    Update AboutMe.js

--- src/AboutMe.js
+++ src/AboutMe.js
@@ -1,25 +1,23 @@
-import { DialogContent, DialogTitle, Dialog, Typography, DialogActions, Button } from '@material-ui/core';
+import { DialogContent,DialogContentText, DialogTitle, Dialog, Typography, DialogActions, Button } from '@material-ui/core';
 import React from 'react';
 import { connect } from 'react-redux';
 import ActionType from './ActionType';
 
 const AboutMe = (props) => {
-    return <Dialog open={props.ui.aboutMe.open}>
+    return <Dialog open={props.ui.aboutMe.open} scroll='paper'>
         <DialogTitle>
             <Typography variant="h4" color="primary">
                 あさごはん512
             </Typography>
         </DialogTitle>
         <DialogContent dividers>
-            <Typography gutterBottom={false}>
-                9つの2択質問を答えて512通りの中から今日のあさごはんを決めましょう。React, Material-UI, Redux, React-Reduxを使用しています。
-            </Typography>
-            <Typography gutterBottom={false}>
-                09/18 Redux, React-Reduxを導入
-            </Typography>
-            <Typography gutterBottom={false}>
-                09/17 初期リリース
-            </Typography>
+            <DialogContentText color="textPrimary">
+                9つの2択質問を答えて512通りの中から今日のあさごはんを決めましょう。
+                <br/>React, Material-UI, Redux, React-Reduxを使用しています。
+                <br/>このWebアプリは勉強のために作成しデプロイしているものなので、予告なく終了する場合があります。
+                <br/>09/18 Redux, React-Reduxを導入
+                <br/>09/17 初期リリース
+            </DialogContentText>
         </DialogContent>
         <DialogActions>
             <Button variant="text" fullWidth onClick={props.close}>閉じる</Button>
